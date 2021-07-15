dab34f62da51cc745ee4d9a4f0622b6a8841c2ba
MNasir14 <muhammad.nasir@asinlabs.tech>
2021-07-15 09:55:34 +0500

    make it better

--- users/user.model.js
+++ users/user.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const schema = new Schema({
     username: { type: String, unique: true, required: true },
     email: { type: String, unique: true, required: true },
-    hash: { type: String, required: true },
+    hash: { type: String, required: false },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     createdDate: { type: Date, default: Date.now }
