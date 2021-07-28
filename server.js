48f791ee9c2ba64ff22a2f9e08d52b99b0ba10ef
MNasir14 <muhammad.nasir@asinlabs.tech>
2021-07-28 09:26:27 +0500

    wakhanda server

--- server.js
+++ server.js
@@ -1,26 +1,60 @@
-﻿require('rootpath')();
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const jwt = require('_helpers/jwt');
-const errorHandler = require('_helpers/error-handler');
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const SocketServer = require('./socketServer')
+const { ExpressPeerServer } = require('peer')
+const path = require('path')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
 
-// use JWT auth to secure the api
-app.use(jwt());
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 
-// api routes
-app.use('/users', require('./users/users.controller'));
 
-// global error handler
-app.use(errorHandler);
+// Socket
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
-// start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
-const server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
-});
+io.on('connection', socket => {
+    SocketServer(socket)
+})
+
+// Create peer server
+ExpressPeerServer(http, { path: '/' })
+
+
+// Routes
+app.use('/api', require('./routes/authRouter'))
+app.use('/api', require('./routes/userRouter'))
+app.use('/api', require('./routes/postRouter'))
+app.use('/api', require('./routes/commentRouter'))
+app.use('/api', require('./routes/notifyRouter'))
+app.use('/api', require('./routes/messageRouter'))
+
+
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if(err) throw err;
+    console.log('Connected to mongodb')
+})
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+
+const port = process.env.PORT || 5000
+http.listen(port, () => {
+    console.log('Server is running on port', port)
+})
